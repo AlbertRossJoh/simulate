8b1677fb95f872c80ecbbda492192798f3ec380c
AlbertRossJoh <93578997+AlbertRossJoh@users.noreply.github.com>
2021-12-11 00:29:15 +0100

    Update simulate.fs

--- simulate.fs
+++ simulate.fs
@@ -2,10 +2,14 @@ module Simulate
 open System
 
 type Drone (startPos : int * int, destPos : int * int, speed : int) = //speed is in cm pr 
-    let mutable (sPos1, sPos2) = startPos //startpo, but mutable
+    let mutable (sPos1, sPos2) = startPos //startpos, but mutable
     let (dPos1, dPos2) = destPos //destpos, but not
-    let mutable isFlying = true
-    member this.IsFlying = isFlying
+    // let mutable isFlying = true
+    let mutable isCrashed = false
+    member this.IsCrashed 
+        with get () = isCrashed
+        and set (a) = isCrashed <- a
+    member this.IsFlying = isCrashed
     member this.Position = (sPos1, sPos2)
     member this.Speed = speed
     member this.Destination = destPos
@@ -21,8 +25,8 @@ type Drone (startPos : int * int, destPos : int * int, speed : int) = //speed is
             let speedXY (c : int) =
                 let (x1, y1) = unitVec lenVec
                 (x1 * (float c), y1 * (float c))
-            sPos1 <- sPos1 + int ((fst (speedXY n))*0.01)
-            sPos2 <- sPos2 + int ((snd (speedXY n))*0.01)
+            sPos1 <- sPos1 + (((fst (speedXY n))*0.01) |> ceil |> int)
+            sPos2 <- sPos2 + (((snd (speedXY n))*0.01) |> ceil |> int)
         else
             sPos1 <- dPos1
             sPos2 <- dPos2
@@ -32,8 +36,8 @@ type Drone (startPos : int * int, destPos : int * int, speed : int) = //speed is
             |> float
             |> sqrt
             |> int
-        if dist * 100 <= int (float this.Speed) && startPos <> (sPos1, sPos2) then //distance is in cm bc. speed is in cm/s
-            isFlying <- if sPos1 = dPos1 && sPos2 = dPos2 then false else true
+        if dist * 100 <= int (float this.Speed) (*&& startPos <> (sPos1, sPos2)*) then //distance is in cm bc. speed is in cm/s
+            // isFlying <- false 
             true
         else 
             (sPos1, sPos2) = (dPos1, dPos2)
@@ -48,22 +52,23 @@ type Airspace () =
         |> float
         |> sqrt
     member this.FlyDrones (allDrones : Drone list)=
-        List.iter (fun (x : Drone) -> x.Fly (int ((float x.Speed)))) allDrones
+        List.iter (fun (x : Drone) -> x.Fly (int (float x.Speed))) allDrones
     member this.AddDrone (x : Drone) =
         droneList <- List.append droneList [x]
-    member this.WillCollide (a : Drone list) (n : int) : (Drone * Drone) list= //one drone gets lucky and escapes
+    member this.WillCollide (a : Drone list) (n : int) : (Drone * Drone) list = //one drone gets lucky and escapes
         let mutable crashed = []
-        for h = 0 to n * 60 do
+        for _ in [0..(n*60)] do
             this.FlyDrones this.Drones
-            for i = 0 to a.Length - 1 do
-                for j = 0 to a.Length - 1 do
-                    if i <> j then
-                        if this.DroneDist this.Drones.[i] this.Drones.[j] <= 5. then
-                            crashed <- (this.Drones.[i], this.Drones.[j]) :: crashed
+            for droneI : Drone in a do
+                for droneJ : Drone in a do
+                    if this.DroneDist droneI droneJ < 5. then
+                        if not droneI.IsCrashed && not droneJ.IsCrashed then
+                            if droneI <> droneJ && crashed |> List.contains (droneI, droneJ) |> not && crashed |> List.contains (droneJ, droneI) |> not then
+                                crashed <- (droneI, droneJ) :: crashed
+                                droneI.IsCrashed <- true
+                                droneJ.IsCrashed <- true
+                        else
+                            crashed <- crashed
+                    else
+                        crashed <- crashed
         crashed                 
-        // for k = 0 to (List.length crashed) - 1 do
-        //     let removeI (x : Drone) = x <> fst crashed.[k]
-        //     let removeJ (x : Drone) = x <> snd crashed.[k]
-        //     for l = 0 to (List.length this.Drones) - 1 do
-        //         droneList <- List.filter removeI this.Drones
-        //         droneList <- List.filter removeJ this.Drones
