d3d0459365e2b33382badff741298203f317391b
AlbertRossJoh <93578997+AlbertRossJoh@users.noreply.github.com>
2021-12-11 17:28:19 +0100

    Create trash.fsx

---
+++ trash.fsx
@@ -0,0 +1,94 @@
+
+let aDrone = Airspace ()
+printf "Adding drones : \n"
+printfn "\tAmount of drones 0 : %b" (0 = (List.length aDrone.Drones))
+printfn "\tAdding drone arg : %A" ((0, 0), (1, 0), 200)
+aDrone.AddDrone (Drone ((0, 0), (1, 0), 200))
+printfn "\tAmount of drones 1 : %b" (1 = (List.length aDrone.Drones))
+printfn "\tAdding drone arg : %A" ((0, 0), (2, 0), 200)
+aDrone.AddDrone (Drone ((0, 0), (2, 0), 200))
+printfn "\tAmount of drones 2 : %b" (2 = (List.length aDrone.Drones))
+printfn "\tAdding drone arg : %A" ((0, 0), (3, 0), 300)
+aDrone.AddDrone (Drone ((0, 0), (3, 0), 300))
+printfn "\tAmount of drones 3 : %b" (3 = (List.length aDrone.Drones))
+printfn "\tAdding drone arg : %A" ((170, 12), (26, 21), 200)
+aDrone.AddDrone (Drone ((170, 12), (26, 21), 200))
+printfn "\tAmount of drones 4 : %b" (4 = (List.length aDrone.Drones))
+printf "Getting drone pos : \n"
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A pos : %A" i aDrone.Drones.[i].Position
+printf "Getting drone dest : \n"
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A dest : %A" i aDrone.Drones.[i].Destination
+printf "Getting drone speed cm/sec : \n"
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A Speed : %A" i aDrone.Drones.[i].Speed
+printf "Flight test : \n"
+aDrone.FlyDrones aDrone.Drones
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A pos : %A" i aDrone.Drones.[i].Position
+printf "Second Flight test : \n"
+aDrone.FlyDrones aDrone.Drones
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A pos : %A" i aDrone.Drones.[i].Position
+printf "Drone crashed : \n"
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A at dest : %b" i (aDrone.Drones.[i].IsCrashed)
+// printf "Drone at dest : \n"
+// for i = 0 to (List.length aDrone.Drones)-1 do
+printfn "\t %A" (List.length (aDrone.WillCollide aDrone.Drones 1))
+printf "Drone dist : \n"
+aDrone.WillCollide aDrone.Drones 1
+printfn "\t %A" (aDrone.DroneDist aDrone.Drones.[2] aDrone.Drones.[3])
+printf "Drone crashed : \n"
+for i = 0 to (List.length aDrone.Drones)-1 do
+    printfn "\tDrone %A at dest : %b" i (aDrone.Drones.[i].IsCrashed)
+
+
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" (aDrone.Drones.[0].Destination)
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// aDrone.Drones.[0].Fly (int((float aDrone.Drones.[1].Speed)))
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// aDrone.Drones.[0].Fly (int((float aDrone.Drones.[1].Speed)))
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// aDrone.Drones.[0].Fly aDrone.Drones.[0].Speed
+
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// aDrone.Drones.[0].Fly aDrone.Drones.[0].Speed
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// aDrone.Drones.[0].Fly aDrone.Drones.[0].Speed
+// printfn "Drone 1 : %b" (aDrone.Drones.[0].AtDestination ())
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// printfn "Drone 2 : %A" aDrone.Drones.[1].Position
+// printfn "Drone 3 : %A" aDrone.Drones.[2].Position
+// aDrone.FlyDrones aDrone.Drones
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// printfn "Drone 2 : %A" aDrone.Drones.[1].Position
+// printfn "Drone 3 : %A" aDrone.Drones.[2].Position
+// aDrone.AddDrone ((324, 435), (34, -213), 34)
+// printfn "Drone 4 : %A" aDrone.Drones.[3].Position
+// aDrone.FlyDrones aDrone.Drones
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// printfn "Drone 1 : %A" aDrone.Drones.[0].IsFlying
+// printfn "Drone 2 : %A" aDrone.Drones.[1].Position
+// printfn "Drone 3 : %A" aDrone.Drones.[2].Position
+// printfn "Drone 4 : %A" aDrone.Drones.[3].Position
+// aDrone.FlyDrones aDrone.Drones
+// printfn "Drone 1 : %A" aDrone.Drones.[0].Position
+// printfn "Drone 2 : %A" aDrone.Drones.[1].Position
+// printfn "Drone 3 : %A" aDrone.Drones.[2].Position
+// printfn "Drone 4 : %A" aDrone.Drones.[3].Position
+// printfn "%A" (Array.length aDrone.Drones)
+// aDrone.AddDrone ((23, 4), (2354, 45), 300)
+// printfn "%A" (Array.length aDrone.Drones)
+// printfn "%A" (aDrone.DroneDist aDrone.Drones.[0] aDrone.Drones.[1])
+// printfn "%A" (aDrone.DroneDist aDrone.Drones.[0] aDrone.Drones.[3])
